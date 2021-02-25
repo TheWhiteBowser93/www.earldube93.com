3bcac8f0b06d77432fc723e44979704b8a3f980f
TheWhiteBowser93 <78491962+TheWhiteBowser93@users.noreply.github.com>
2021-02-25 18:02:29 -0500

    Create index.php
    homepage

---
+++ .github/workflows/index.php
@@ -0,0 +1,200 @@
+<?php include('head.php'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="header">
+				<h1> - Earl Dube - </h1>
+				<h4>Video Editor, 3D Animator</h4>
+
+			</div>
+    	</div>
+	</div>
+
+	<!--Navbar-->
+    <?php require_once('navbar.php'); ?>
+
+    <!--Demo Reel-->
+    <div class="row">
+        <div class="col-md-12">
+            <div class="subbody">
+                <h1>Earl Dube Demo Reel 2020</h1>
+                <div class="mainvideo2">
+                    <div class="embed-responsive embed-responsive-16by9">
+						<iframe src="https://www.youtube.com/embed/_faeCZRufjo" frameborder="0" allow="accelerometer;  picture-in-picture;" allowfullscreen></iframe>
+					</div>
+                </div>
+                <div style="margin-top: 10px;">
+                    <h4>My best video editing and 3D animation work I've done all rolled into one video with some awesome music to beat!</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	<!--Carousel-->
+	<div class="row">
+			<div class="col-md-12">
+				<div class="subbody">
+					<h1>Earl Dube 3D Animation Demo Reel 2018</h1>
+				</div>
+			</div>
+	</div>
+
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="11"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="12"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="13"></li>
+		</ol>
+			 <div class="carousel-inner">
+
+				<div class="carousel-item active">
+					<img src="/assets/images/Demo-Reel/models/Carnipede-Comp.png" alt="Carnipede" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+				  <img src="/assets/images/Demo-Reel/models/gunship-comp2.png" alt="gunship" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+				  <img src="/assets/images/Demo-Reel/models/Junk Warrior comp.jpg" alt="Junk" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Metroid-Ridley-Showcase2.png" alt="Ridley" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Neo Ridley comp.jpg" alt="Neo" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Nitro Warrior comp.jpg" alt="Nitro" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Speed Warrior comp.jpg" alt="Speed" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Stardust Dragon comp.jpg" alt="Stardust" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Zazabi comp.jpg" alt="Zaza" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Zebesian Pirate comp.jpg" alt="pirate" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Assault Dog comp.jpg" alt="dog" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Arachnus comp.jpg" alt="Arach" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Yakuza comp.jpg" alt="Stardust" class="d-block w-100">
+				</div>
+
+				<div class="carousel-item">
+					<img src="/assets/images/Demo-Reel/models/Red Dragon Archfiend comp.jpg" alt="Archfiend" class="d-block w-100">
+				</div>
+
+
+			 </div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+	</div>
+
+	<div class="row">
+		<div class="subbody">
+			<div style="margin-top: 10px;">
+				<h4>The best of the best 3D modeling and animation work I've done in the past 4 years all rolled into one awesome slideshow!</h4>
+			</div>
+		</div>
+	</div>
+
+
+	<!--Gifs-->
+	<div class="row">
+        <div class="col-md-6">
+            <div class="header2">
+                <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/musicvideos.php">Music Videos<img src="/assets/images/gifs/TheWindRises.gif" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="header2">
+                <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/project.php">Bioweapon Wars<img src="/assets/images/gifs/BioweaponWars.gif" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+        </div>
+	</div>
+
+	<div class="row">
+    	<div class="col-md-6">
+            <div class="header2">
+              <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/trailers.php">Trailers<img src="/assets/images/gifs/terminator.gif" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+        </div>
+      	<div class="col-md-6">
+            <div class="header2">
+              <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/film.php">Film Projects<img src="/assets/images/gifs/videography2.gif" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+		</div>
+	</div>
+
+	<div class="row">
+        <div class="col-md-6">
+            <div class="header2">
+              <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/video.php">Videography<img src="/assets/images/gifs/video.gif" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="header2">
+              <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/model.php">3D modeling<img src="/assets/images/gifs/model.gif" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+        </div>
+	</div>
+
+	<div class="row">
+        <div class="col-md-6">
+            <div class="header2">
+              <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/about.php">About me<img src="/assets/images/pictures/earldube93.png" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="header2">
+              <a href="https://<?php echo($_SERVER['HTTP_HOST']); ?>/social.php">Social<img src="/assets/images/pictures/socialbanner2.png" alt="HTML5 Icon" class="d-block w-100"></a>
+            </div>
+		</div>
+	</div>
+
+
+
+
+
+<!--footer-->
+<?php include('footer.php'); ?>
